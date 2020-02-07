f607b178aab4707832dcf5ccb1045d5f96d4ac74
er.or.ignore <36531908+errorignore@users.noreply.github.com>
2020-02-07 19:44:24 +0200

    Initial commit

---
+++ assets/errorignore/js/err_cleaner.js
@@ -0,0 +1,28 @@
+//cards.search
+$(document).ready(function() {
+    $("#myINPUTSEARCH1").on("keydown", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myDIVCARDS div.col-md-3").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+//cards.search
+$(document).ready(function() {
+    $("#err_search").on("keydown", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myDIVCARDS div.col-md-3").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+//root.remove
+$(document).ready(function() {
+    $(".err-ng-streem").remove();
+});
+
+$(document).ready(function() {
+$("div.err-search-button").click(function() {
+$("div.cards-catalog").toggle();
+});
+});
